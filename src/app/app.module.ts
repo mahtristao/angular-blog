578e5aa29cb32e2879324e5eb125aea87fe1aaa1
Mah Tristao <marjietristao@gmail.com>
2023-10-12 00:27:11 +0200

    created components und pages/home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { SecondTitleComponent } from './components/second-title/second-title.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    CardComponent,
+    FooterComponent,
+    CategoriesComponent,
+    SecondTitleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/second-title/second-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-title',
+  templateUrl: './second-title.component.html',
+  styleUrls: ['./second-title.component.css']
+})
+export class SecondTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
